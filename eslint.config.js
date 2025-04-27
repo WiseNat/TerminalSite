32e2b27d5cb3550727239b11fbed3710f7a173cb
Nathan Wise <nathan88wise@gmail.com>
2025-04-27 16:44:31 +0100

    chore: configure npm tech stack packages

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,28 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import css from "@eslint/css";
-import { defineConfig } from "eslint/config";
-
+import {defineConfig, globalIgnores} from "eslint/config";
+import sonarjs from 'eslint-plugin-sonarjs';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        plugins: {js, sonarjs},
+        extends: ["js/recommended"],
+        languageOptions: {globals: globals.browser}
+    },
+    {
+        files: ["**/*.jsonc"],
+        plugins: {json},
+        language: "json/jsonc",
+        extends: ["json/recommended"]
+    },
+    {
+        files: ["**/*.css"],
+        plugins: {css},
+        language: "css/css",
+        extends: ["css/recommended"]
+    },
+    ...tseslint.configs.recommended,
+    globalIgnores(["dist/"])
 ]);
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from "vite";
+import autoprefixer from "autoprefixer";
+
+export default defineConfig({
+  plugins: [],
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
+  test: {},
+});
