462b80d93cd00d403ac1e9b1397fabcaa15edba3
Nathan Wise <nathan88wise@gmail.com>
2025-04-27 23:00:17 +0100

    style: apply Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -3,28 +3,28 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import json from "@eslint/json";
 import css from "@eslint/css";
-import {defineConfig, globalIgnores} from "eslint/config";
-import sonarjs from 'eslint-plugin-sonarjs';
+import { defineConfig, globalIgnores } from "eslint/config";
+import sonarjs from "eslint-plugin-sonarjs";
 
 export default defineConfig([
-    {
-        files: ["**/*.{js,mjs,cjs,ts}"],
-        plugins: {js, sonarjs},
-        extends: ["js/recommended"],
-        languageOptions: {globals: globals.browser}
-    },
-    {
-        files: ["**/*.jsonc"],
-        plugins: {json},
-        language: "json/jsonc",
-        extends: ["json/recommended"]
-    },
-    {
-        files: ["**/*.css"],
-        plugins: {css},
-        language: "css/css",
-        extends: ["css/recommended"]
-    },
-    ...tseslint.configs.recommended,
-    globalIgnores(["dist/"])
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js, sonarjs },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
+  ...tseslint.configs.recommended,
+  globalIgnores(["dist/"]),
 ]);
