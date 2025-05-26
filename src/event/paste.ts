e6f26301092201d3caedab3c4a3df6b4e41829f0
Nathan Wise <nathan88wise@gmail.com>
2025-05-26 13:42:08 +0100

    refactor: change rich format pasting prevention to use execCommand

--- src/event/paste.ts
+++ src/event/paste.ts
@@ -9,29 +9,12 @@
  */
 export function paste(event: ClipboardEvent) {
   event.preventDefault();
+
   const text = event.clipboardData?.getData("text/plain") ?? "";
-  insertTextAtCursor(text);
-}
 
-/**
- * Inserts text at the user's cursor position.
- *
- * @param text text to insert
- */
-function insertTextAtCursor(text: string) {
-  const selection = window.getSelection();
-  if (!selection?.rangeCount) {
-    return;
-  }
-
-  const textNode = document.createTextNode(text);
-
-  const range = selection.getRangeAt(0);
-  range.deleteContents();
-  range.insertNode(textNode);
-  range.setStartAfter(textNode); // Move the caret after the inserted text node
-  range.collapse(true);
-
-  selection.removeAllRanges();
-  selection.addRange(range);
+  /*
+   * Deprecated though no good alternatives exist for pasting with proper undo history
+   * When `contenteditable="plaintext-only"` becomes more mainstream, this can be removed.
+   */
+  document.execCommand("insertHTML", false, text);
 }
