0a1927af1db972b1ec5e63790ae2e17366df8f78
Nathan Wise <nathan88wise@gmail.com>
2025-06-19 21:07:00 +0100

    fix: pasting prevention in readonly text in WebKit Desktop & Mobile browsers

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 // TODO: Proper E2E testing with Selenium/Playwright/Cypress
 
 import TerminalUtil from "./util/terminal_util.ts";
+import * as Bowser from "bowser";
 
 const terminal = TerminalUtil.getTerminal();
 
@@ -17,14 +18,33 @@ const readonlyIndex = TerminalUtil.getTerminalContent().length;
 let contentBeforeInput = "";
 
 terminal.addEventListener("paste", (event) => {
-  event.preventDefault();
+  const browser = Bowser.getParser(window.navigator.userAgent);
 
-  contentBeforeInput = TerminalUtil.getTerminalContent();
+  /*
+   * Firefox recently added support for contentEditable="plaintext-only". exeCommand safety net is being used to ensure
+   * users with older Firefox browsers cannot paste rich formatted text.
+   *
+   * We only need to run the below for Firefox (+ fork) browsers but detecting those are a nightmare as Firefox is not
+   * classified as an engine in Bowser; I'd need to know the browser name of every fork.
+   *
+   * Instead, as execCommand ONLY fails in Webkit, and it's easy to detect Webkit browsers as they're engines, the below
+   * will run on every non-Webkit browser.
+   *
+   * This can be removed in 5-10 years time as contentEditable="plaintext-only" support will be in most Firefox users
+   * browsers.
+   */
 
-  // Deprecated though no good alternatives exist for pasting with proper undo history.
-  // When `contenteditable="plaintext-only"` becomes more mainstream, this can be removed.
-  const text = event.clipboardData?.getData("text/plain") ?? "";
-  document.execCommand("insertText", false, text);
+  // Should work based on ENGINE_MAP in https://github.com/bowser-js/bowser/blob/master/src/constants.js
+  if (browser.getEngineName() !== "WebKit") {
+    event.preventDefault();
+
+    // Added in case beforeinput does not fire from insertText call - varies per browser
+    contentBeforeInput = TerminalUtil.getTerminalContent();
+
+    // Deprecated though no good alternatives exist for pasting with proper undo history.
+    const text = event.clipboardData?.getData("text/plain") ?? "";
+    document.execCommand("insertText", false, text);
+  }
 });
 
 terminal.addEventListener("beforeinput", () => {
