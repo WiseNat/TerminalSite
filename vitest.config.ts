cba79eb164f157470e72c96b9bb5322b46114ab4
Nathan Wise <nathan88wise@gmail.com>
2025-07-01 16:48:57 +0100

    chore: replace deprecated workspace with projects in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,23 +2,23 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    workspace: [
-      {
+    projects: [
+      defineConfig({
         test: {
           name: "Unit",
           include: ["test/unit/**/*.test.ts"],
           environment: "node",
           clearMocks: true,
         },
-      },
-      {
+      }),
+      defineConfig({
         test: {
           name: "Integration",
           include: ["test/integration/**/*.test.ts"],
           environment: "jsdom",
           clearMocks: true,
         },
-      },
+      }),
     ],
   },
 });
