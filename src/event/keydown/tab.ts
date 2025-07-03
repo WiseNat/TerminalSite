a0a9f06b7dc145bd1be114d317df21903019f897
Nathan Wise <nathan88wise@gmail.com>
2025-07-03 18:38:48 +0100

    feat: default autocompletion

--- src/event/keydown/tab.ts
+++ src/event/keydown/tab.ts
@@ -1,5 +1,103 @@
+import TerminalUtil from "../../util/terminal_util.ts";
+import CommandUtil from "../../util/command_util.ts";
+import getCommandScripts, {
+  removePathFromCommandScriptKey,
+} from "../../util/meta_import_util.ts";
+import { userPrompt } from "../../constant/prompt.ts";
+
 // TODO: JSDoc
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function processTab(_event: KeyboardEvent) {
-  // TODO: impl
+/**
+ * Processes the 'Tab' key event. This will perform autocompletion of values in the terminal, either
+ * 1. With a custom command specific autocomplete
+ * 2. With a default autocomplete with command and directories as values
+ *
+ * @param event
+ */
+export function processTab(event: KeyboardEvent) {
+  event.preventDefault();
+
+  const userInput = TerminalUtil.getUserInput();
+  if (userInput == "") {
+    // We don't want suggestions provided when nothing exists in the user input
+    return;
+  }
+
+  const tokenisedCommand = CommandUtil.tokenise(userInput);
+
+  let values: string[];
+
+  if (tokenisedCommand.args.length != 0) {
+    // TODO: IF command has a custom autocomplete, call that, ELSE suggest directories & filenames
+    values = [];
+  } else {
+    values = getCommandSuggestions(tokenisedCommand.name);
+    // TODO: suggest directories when functionality for them is added, Bash suggests those despite them not working so
+    //  that should be mirror-ed here.
+  }
+
+  autocomplete(values, userInput);
+}
+
+/**
+ * Autocompletes dependent on the provided values.
+ *
+ * IF there are no values -> nothing happens
+ *
+ * IF there is 1 value -> that value will be autocompleted
+ *
+ * IF more than 1 value exists -> all values will be printed to the user
+ *
+ * @param values values to be considered for autocompletion, can be empty
+ * @param userInput current user input, used to dictate what text will be appended when autocompleting.
+ */
+function autocomplete(values: string[], userInput: string) {
+  // Do nothing when there are no suggestions
+  if (values.length == 0) {
+    return;
+  }
+
+  console.info(`Suggested values are '${values}'`);
+
+  // Autocomplete value if there's only 1 suggestion, otherwise output all suggestions
+  if (values.length == 1) {
+    let suggestedValue = values[0];
+
+    // We want to append whitespace to the end of a command if it's missing, but we don't want to append any additional
+    // whitespace after that, e.g. going from "command⸱" to "command⸱⸱"
+    if (`${suggestedValue} ` === userInput) {
+      return;
+    }
+
+    // Should search left to right, finding "userInput" at the start of "suggestedValue"
+    suggestedValue = suggestedValue.replace(userInput, "");
+
+    console.info(`Autocompleting with '${suggestedValue}'`);
+    TerminalUtil.appendText(`${suggestedValue} `);
+  } else {
+    console.info("Providing a list of suggested autocompletion values");
+
+    // TODO: Make this use TUI (columns) when that's implemented
+    TerminalUtil.appendText(`\n${values.join("\t")}\n${userPrompt}`);
+    TerminalUtil.updateReadOnlyIndex();
+    TerminalUtil.appendText(userInput);
+  }
+}
+
+/**
+ * Gets a list of suggested commands for autocompletion which start with the same value as the `searchValue`.
+ *
+ * @param searchValue the value to search against
+ * @returns a list of command suggestions
+ */
+function getCommandSuggestions(searchValue: string): string[] {
+  const commandSuggestions = [];
+
+  for (const commandScript in getCommandScripts()) {
+    const pathlessCommandScript = removePathFromCommandScriptKey(commandScript);
+    if (pathlessCommandScript.startsWith(searchValue)) {
+      commandSuggestions.push(pathlessCommandScript);
+    }
+  }
+
+  return commandSuggestions;
 }
--- test/integration/event/keydown/tab.test.ts
+++ test/integration/event/keydown/tab.test.ts
@@ -1,25 +1,178 @@
-import { describe, test } from "vitest";
+import { beforeEach, describe, expect, test, vi } from "vitest";
 import { processTab } from "../../../../src/event/keydown/tab";
+import TerminalUtil from "../../../../src/util/terminal_util";
+import { defaultPrompt } from "../../../e2e/helper/constant/generic";
+import getCommandScripts from "../../../../src/util/meta_import_util";
+import { unmock } from "../../Unmock";
+
+// TODO: E2E tests
 
 describe("Tab", () => {
-  // TODO: implement tests
-  test.todo("Autocompletes a command name when typing a command", () => {
-    // Arrange
+  describe("processTab", () => {
+    // Spy
+    const appendText = vi.spyOn(TerminalUtil, "appendText");
+
+    // Mock
+    vi.mock("../../../../src/util/terminal_util");
+    vi.mock("../../../../src/util/meta_import_util");
+
+    // Other
     const event = new KeyboardEvent("keydown");
 
-    // Act
-    processTab(event);
+    beforeEach(async () => {
+      await unmock(
+        "../../src/util/meta_import_util",
+        "removePathFromCommandScriptKey",
+      );
+    });
 
-    // Assert
-    // TODO: ????
+    test.todo("Autocompletes a command name when typing a command", () => {
+      // Arrange
+      const userInput = "ech";
+      vi.mocked(TerminalUtil.getUserInput).mockReturnValue(userInput);
+
+      // Act
+      processTab(event);
+
+      // Assert
+      // TODO: Assert somehow. Negative assert based on method that passes to the owning command?
+    });
+
+    test.todo("Autocompletes a directory when typing a directory", () => {
+      // Arrange
+      const userInput = "~/Des";
+      vi.mocked(TerminalUtil.getUserInput).mockReturnValue(userInput);
+
+      // Act
+      processTab(event);
+
+      // Assert
+      // TODO: Assert somehow. Negative assert based on method that passes to the owning command?
+    });
+
+    test.todo(
+      "Passes autocompletion to the owning command when a command has been typed",
+    );
+
+    describe("Default autocompletion", () => {
+      test("provides valid suggestions", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("ech");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).toHaveBeenCalledWith(
+          `\necho\techoing\techo_ers\n${defaultPrompt}`,
+        );
+      });
+
+      test("does nothing when there are no valid suggestions", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("foo");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).not.toHaveBeenCalled();
+      });
+
+      test("automatically inserts when there's only a single valid suggestion", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("echo_");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).toHaveBeenCalledWith("ers ");
+      });
+
+      test("adds a space when the command name is already typed and there's only a single valid suggestion", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("echoing");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).toHaveBeenCalledWith(" ");
+      });
+
+      test("does nothing when the command name is already typed with a space and there's only a single valid suggestion", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("echoing ");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).not.toHaveBeenCalled();
+      });
+
+      test("provides valid suggestions when a command name is already typed and there's other suggestions", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("echo");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).toHaveBeenCalledWith(
+          `\necho\techoing\techo_ers\n${defaultPrompt}`,
+        );
+      });
+
+      test("does nothing when nothing is typed", () => {
+        // Arrange
+        vi.mocked(TerminalUtil.getUserInput).mockReturnValue("");
+        vi.mocked(getCommandScripts).mockReturnValue({
+          "/src/command/scripts/echo.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echoing.ts": { default: { run: vi.fn() } },
+          "/src/command/scripts/echo_ers.ts": { default: { run: vi.fn() } },
+        });
+
+        // Act
+        processTab(event);
+
+        // Assert
+        expect(appendText).not.toHaveBeenCalled();
+      });
+    });
+
+    // TODO: tests for this
+    describe.todo("Custom command autocompletion");
   });
-
-  test.todo(
-    "Passes autocompletion to the owning command when a command has been typed",
-  );
-
-  // TODO: Implement this when working directories is added
-  test.todo(
-    "Defaults to file autocompletion when a command that does not exist is typed",
-  );
 });
