6305cafa9429cc890cc47378722fd5c3d35a6f97
Nathan Wise <nathan88wise@gmail.com>
2025-07-04 02:06:28 +0100

    test: clicking the boundaries of the page focuses the terminal

--- test/e2e/spec/accessibility.spec.ts
+++ test/e2e/spec/accessibility.spec.ts
@@ -1,12 +1,13 @@
 import { expect, test } from "../fixture";
 import AxeBuilder from "@axe-core/playwright";
+import { terminalSelector } from "../helper/constant/generic";
 
 test.describe("homepage", () => {
   test("should not have any automatically detectable accessibility issues", async ({
     page,
   }, testInfo) => {
     // Disabled rules:
-    // - 'page-has-heading-one' - h1 is used by screenreaders to quickly skip to the main content, this is redundant for my site
+    // - 'page-has-heading-one' - h1 is used by screen-readers to quickly skip to the main content, this is redundant for my site
     const accessibilityScanResults = await new AxeBuilder({ page })
       .disableRules(["page-has-heading-one"])
       .analyze();
@@ -32,5 +33,34 @@ test.describe("homepage", () => {
     });
 
     expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("focus", () => {
+  test("clicking anywhere on the web page focuses the terminal", async ({
+    page,
+  }) => {
+    // Arrange
+    const viewportSize = page.viewportSize();
+    const maxWidth = viewportSize!.width - 1;
+    const maxHeight = viewportSize!.height - 1;
+    const minWidth = 0;
+    const minHeight = 0;
+    const corners = [
+      { x: minWidth, y: minHeight, type: "Top Left" },
+      { x: minWidth, y: maxHeight, type: "Bottom Left" },
+      { x: maxWidth, y: minHeight, type: "Top Right" },
+      { x: maxWidth, y: maxHeight, type: "Bottom Right" },
+    ];
+
+    // Act & Assert
+    const terminal = page.locator(terminalSelector);
+    for (const corner of corners) {
+      await page.mouse.click(corner.x, corner.y);
+      await expect(
+        terminal,
+        `Page clicked in the ${corner.type} to have the terminal focussed`,
+      ).toBeFocused();
+    }
   });
 });
