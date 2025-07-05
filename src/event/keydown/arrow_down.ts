446d5a37ff60829010540acaffa8356bf602d595
Nathan Wise <nathan88wise@gmail.com>
2025-07-05 18:11:30 +0100

    chore: rough command history file structure

---
+++ src/event/keydown/arrow_down.ts
@@ -0,0 +1,9 @@
+/**
+ * Processes the 'ArrowDown' key event. TODO
+ *
+ * @param event event listener {@link KeyboardEvent}
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function processArrowDown(_event: KeyboardEvent) {
+  // TODO: impl
+}
---
+++ src/event/keydown/arrow_up.ts
@@ -0,0 +1,9 @@
+/**
+ * Processes the 'ArrowUp' key event. TODO
+ *
+ * @param event event listener {@link KeyboardEvent}
+ */
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function processArrowUp(_event: KeyboardEvent) {
+  // TODO: impl
+}
