724ab4b7093955998fd6f996aa8a1ad83d07f5c3
Nathan Wise <nathan88wise@gmail.com>
2025-08-06 23:24:37 +0100

    fix: focus input element when clicking anywhere on the site

---
+++ src/event/click.ts
@@ -0,0 +1,19 @@
+import TerminalUtil from "../util/terminal_util.ts";
+
+/**
+ * Event listener function for handling click events in the terminal.
+ *
+ * @param event event listener {@link MouseEvent}
+ */
+export function click(event: MouseEvent) {
+  const input = TerminalUtil.getInputElement();
+
+  if (input.contains(event.target as Node)) {
+    return;
+  }
+
+  event.preventDefault();
+
+  input.focus();
+  TerminalUtil.cursorToEnd();
+}
--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,18 @@ import { paste } from "./event/paste.ts";
 import { keydown } from "./event/keydown.ts";
 import { initialPrompt, userPrompt } from "./constant/prompt.ts";
 import FileSystemUtil from "./util/file_system_util.ts";
+import { click } from "./event/click.ts";
 
+// Terminal Set-up
 FileSystemUtil.setHomeDirectory("/home/nathanwise");
 FileSystemUtil.setCurrentWorkingDirectory("~");
 
 TerminalUtil.setOutput(initialPrompt);
 TerminalUtil.setPrompt(userPrompt);
 TerminalUtil.setInput("");
+
+// Event Listeners
+document.addEventListener("click", click);
 
 const inputElement = TerminalUtil.getInputElement();
 inputElement.addEventListener("paste", paste);
