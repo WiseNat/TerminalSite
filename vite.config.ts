a2edde173b9ed10b5c49ce7fb004e889a96a579e
Nathan Wise <nathan88wise@gmail.com>
2025-09-12 01:06:06 +0100

    chore: add rollup visualiser

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import autoprefixer from "autoprefixer";
 import FileTree from "./src/plugins/vite_plugin_file_tree";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [FileTree("src/content", "home")],
+    plugins: [
+      FileTree("src/content", "home"),
+      visualizer({ brotliSize: true, gzipSize: true }) as PluginOption,
+    ],
     css: {
       postcss: {
         plugins: [autoprefixer({})],
