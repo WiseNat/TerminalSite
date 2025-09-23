d97293f1647a4c3d56b20519d6325f0f1333af85
Nathan Wise <nathan88wise@gmail.com>
2025-09-23 22:46:59 +0100

    feat: uname command

--- src/constant/system.ts
+++ src/constant/system.ts
@@ -1,3 +1,8 @@
 export const HOSTNAME = "nathan-wise-portfolio";
 export const DOMAIN = "localdomain";
 export const IP_ADDRESS = "127.0.0.1";
+export const KERNEL_NAME = "Linux";
+export const KERNEL_RELEASE = "5.17.15-generic";
+export const KERNEL_VERSION = "#1 PREEMPT Tue May 17 21:11:00 UTC 2022";
+export const ARCHITECTURE = "x86_64";
+export const OPERATING_SYSTEM = "GNU/Linux";
---
+++ test/unit/src/command/scripts/uname.test.ts
@@ -0,0 +1,161 @@
+import { describe, expect, test, vi } from "vitest";
+import TerminalUtil from "../../../../../src/util/terminal_util.ts";
+import UNAME from "../../../../../src/command/scripts/uname.ts";
+
+describe("Hostname", () => {
+  // Spy
+  const appendOutput = vi.spyOn(TerminalUtil, "appendOutput");
+
+  // Mock
+  vi.mock("../../../../../src/util/terminal_util");
+
+  // Other
+  const kernelName = "Linux";
+  const nodeName = "nathan-wise-portfolio";
+  const kernelRelease = "5.17.15-generic";
+  const kernelVersion = "#1 PREEMPT Tue May 17 21:11:00 UTC 2022";
+  const machine = "x86_64";
+  const processor = "x86_64";
+  const hardwarePlatform = "x86_64";
+  const operatingSystem = "GNU/Linux";
+
+  describe("run", () => {
+    test("should output the kernel name when no arguments are provided", async () => {
+      // Arrange
+      const args: string[] = [];
+
+      // Act
+      await UNAME.run(args);
+
+      // Assert
+      expect(appendOutput).toHaveBeenCalledExactlyOnceWith(`\n${kernelName}`);
+    });
+
+    ["-a", "--all"].forEach((flag) => {
+      test(`Given the ${flag}, should output all other flag contents in order`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(
+          `\n${kernelName} ${nodeName} ${kernelRelease} ${kernelVersion} ${machine} ${processor} ${hardwarePlatform} ${operatingSystem}`,
+        );
+      });
+    });
+
+    ["-s", "--kernel-name"].forEach((flag) => {
+      test(`Given the ${flag}, should output the kernel name`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(`\n${kernelName}`);
+      });
+    });
+
+    ["-n", "--nodename"].forEach((flag) => {
+      test(`Given the ${flag}, should output the node name`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(`\n${nodeName}`);
+      });
+    });
+
+    ["-r", "--kernel-release"].forEach((flag) => {
+      test(`Given the ${flag}, should output the kernel release`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(
+          `\n${kernelRelease}`,
+        );
+      });
+    });
+
+    ["-v", "--kernel-version"].forEach((flag) => {
+      test(`Given the ${flag}, should output the kernel version`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(
+          `\n${kernelVersion}`,
+        );
+      });
+    });
+
+    ["-m", "--machine"].forEach((flag) => {
+      test(`Given the ${flag}, should output the machine`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(`\n${machine}`);
+      });
+    });
+
+    ["-p", "--processor"].forEach((flag) => {
+      test(`Given the ${flag}, should output the processor`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(`\n${processor}`);
+      });
+    });
+
+    ["-i", "--hardware-platform"].forEach((flag) => {
+      test(`Given the ${flag}, should output the hardware platform`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(
+          `\n${hardwarePlatform}`,
+        );
+      });
+    });
+
+    ["-o", "--operating-system"].forEach((flag) => {
+      test(`Given the ${flag}, should output the operating system`, async () => {
+        // Arrange
+        const args: string[] = [flag];
+
+        // Act
+        await UNAME.run(args);
+
+        // Assert
+        expect(appendOutput).toHaveBeenCalledExactlyOnceWith(
+          `\n${operatingSystem}`,
+        );
+      });
+    });
+  });
+});
